8efb3b07b642802953d4d91eb34b1ee3d1251c35
Matt Marini <mattmarini8765@gmail.com>
2023-04-01 11:33:17 +1100

    Made rock paper scissors game that lasts 1 round.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 function getComputerChoice(min, max) {
     let randomnumber = Math.floor(Math.random() * (max - min)) + min;
-    console.log(randomnumber);
 
     let choice = null;
 
@@ -20,4 +19,46 @@ function getComputerChoice(min, max) {
     return choice;
 }
 
-getComputerChoice(1, 4);
+
+// Player selection
+
+function getPlayerChoice() {
+    let playerchoice = prompt("Choose Rock, Paper or Scissors");
+    let choice = playerchoice.toLowerCase();
+    console.log(choice);
+    return choice;
+}
+
+// Play a round
+
+function playRound(player, computer) {
+    if (player === "rock" && computer === "scissors") {
+        console.log("You win! Rock crushes scissors!");
+    }
+    else if (player === "rock" && computer === "rock") {
+        console.log("Tie! You both selected rock.");
+    }
+    else if (player === "rock" && computer === "paper") {
+        console.log("You lose! Paper covers rock.");
+    }
+    else if (player === "paper" && computer === "scissors") {
+        console.log("You lose! Scissors cuts paper.");
+    }
+    else if (player === "paper" && computer === "rock") {
+        console.log("You win! Paper covers rock.");
+    }
+    else if (player === "paper" && computer === "paper") {
+        console.log("Tie! You both selected paper.");
+    }
+    else if (player === "scissors" && computer === "scissors") {
+        console.log("Tie! You both selected scissors.");
+    }
+    else if (player === "scissors" && computer === "rock") {
+        console.log("You lose! rock crushes scissors.");
+    }
+    else if (player === "scissors" && computer === "paper") {
+        console.log("You win! Scissors cuts paper!");
+    }
+}
+
+playRound (getPlayerChoice(), getComputerChoice(1, 4));
